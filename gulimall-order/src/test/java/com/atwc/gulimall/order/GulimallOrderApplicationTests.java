26d16c81790f072a0f3ee635a65027533bef7436
weichen <29344496@qq.com>
2022-10-18 15:52:42 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/atwc/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwc.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/test/java/com/atwc/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwc.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
